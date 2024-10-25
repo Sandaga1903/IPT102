b0b47a3ee2d97d472eb5977bd1a37a973ed6b4f8
Sandaga1903 <143481496+Sandaga1903@users.noreply.github.com>
2024-10-25 17:07:06 +0800

    Delete Sandaga_Solution directory

--- Sandaga_Solution/Database1/table/Table DEPT_LOCATIONS.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table DEPT_LOCATIONS]
-(
-	[Id] INT NOT NULL , 
-    [(Dnumber] INT NOT NULL, 
-    [Dlocation] VARCHAR(15) NOT NULL, 
-    PRIMARY KEY ([Dlocation, Dnumber]), 
-    CONSTRAINT [FK_Table DEPT_LOCATIONS_ToTable] FOREIGN KEY ([Dnumber]) REFERENCES [Department] ( [Dnumber] ));
-)
